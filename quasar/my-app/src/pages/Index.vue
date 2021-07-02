20b857b3cbb55df7361645b6c09942711a3fdec3
Eric <eric@naguras.com>
2021-07-02 09:48:51 +0000

    initial commit

---
+++ quasar/my-app/src/pages/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+     <p v-for="i in 20" :key="i">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+      ratione atque asperiores, sunt veritatis consequuntur sit consectetur odit
+      corrupti optio velit saepe laborum cupiditate, in neque nemo molestiae at
+      error?
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
---
+++ vue3/my-vite-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <p v-for="i in 20" :key="i">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+      ratione atque asperiores, sunt veritatis consequuntur sit consectetur odit
+      corrupti optio velit saepe laborum cupiditate, in neque nemo molestiae at
+      error?
+    </p>
+  </div>
+</template>
+
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  // This starter template is using Vue 3 experimental <script setup> SFCs
+  // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
